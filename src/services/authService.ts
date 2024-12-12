6659dfacc5e3eae346f15f7f473f9f8ba2c633a6
Manoel Prado <manoelprado.aecjr@gmail.com>
2024-12-12 00:24:36 -0300

    use getUserByEmail method

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,9 +2,10 @@ import User from '../models/User';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { ENV } from '../config/env'
+import { getUserByEmail } from './userService';
 
 export const loginUser = async (email: string, password: string) => {
-  const user = await User.findOne({ where: { email } });
+  const user = await getUserByEmail(email);
 
   if (!user) {
     throw new Error('User not found');
